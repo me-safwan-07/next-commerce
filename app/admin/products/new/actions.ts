1e22111a45060006f05cafd45afe78f41745e52f
me-safwan-07 <mesafwan07@gmail.com>
2025-07-10 23:28:19 +0530

    deleted the old design and features

--- app/admin/products/new/actions.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { createProduct } from "@/lib/product/service";
-import { TProduct, TProductInput } from "@/types/product";
-
-export const CreateProductAction = async (data: TProductInput): Promise<TProduct> => {
-    return await createProduct(data)
-}
